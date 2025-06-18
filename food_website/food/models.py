36d2db195178e5b57c8812007708e0916c27efa5
Sannan Ahmad <sannanahmad.16@gmail.com>
2025-06-18 19:15:24 +0500

    IDK WHAT I DID

--- food_website/food/models.py
+++ food_website/food/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class FoodInventory(models.Model):
-    pass
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=255)
+    price = models.IntegerField()
+    
