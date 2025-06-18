5e5b0e875c0f1ac867ce371fe0257e716f7c561e
Sannan Ahmad <sannanahmad.16@gmail.com>
2025-06-18 22:22:03 +0500

    Learned Templating in Django

--- food_website/food/urls.py
+++ food_website/food/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('<int:item_id>/',   views.item, name='item'),
     path('item', views.item, name='item')
 ]
--- food_website/food/views.py
+++ food_website/food/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Item
+from django.template import loader
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    items = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+        'items' : items,
+        'name' : 'Sannan'
+    }
+    return render(request, 'food/index.html', context)
 
-def item(request):
-    return HttpResponse("<h1>This is my item view niggaaaaaaaaaaaaaaaa<h1/>")
+def item(request, item_id):
+    return HttpResponse(f"The id of this item is {item_id}")
